ed4d12402bc9c79b8dc66bfa614c93cc4f712739
CuiYang16 <cuiyangmail@163.com>
2018-10-22 18:11:01 +0800

    c

--- src/main/resources/static/js/carousel.js
+++ src/main/resources/static/js/carousel.js
@@ -8,7 +8,8 @@ function carouselImg() {
 	var carouselimg = document.getElementById("carouselimg");
 	var img = carouselimg.getElementsByTagName("img");
 	for(var i=0;i<img.length;i++){
-		img[i].style.height = iheight * 0.7 + "px";
+		img[i].style.height = iheight * 0.4 + "px";
+	
 		img[i].style.width = iwidth * 1 + "px";
 	}
 }
